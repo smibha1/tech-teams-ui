8617e7f956bc133dd928d580185525d8c58701b3
Jake Perez <jacobanthony.perez@gmail.com>
2017-12-01 10:13:38 -0800

    [ops] - integrate jest tests - add yarn commit pre-hook

---
+++ __tests__/math.test.js
@@ -0,0 +1,9 @@
+const operations = require('../test/math');
+
+test('should properly add numbers', () => {
+  expect(operations.add(1, 3)).toBe(4);
+});
+
+test('should subtract numbers', () => {
+  expect(operations.subtract(5, 10)).toBe(-5);
+});
