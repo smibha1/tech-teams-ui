1e7fcf22c83c7c0750f102aee99620d2026d2d37
“Smita <smitajbhakta@gmail.com>
2017-12-10 10:07:51 -0800

    [feat] -Landing page navbar with login and signup

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -4,10 +4,32 @@ import './LandingPage.css';
 class LandingPage extends React.Component {
   render() {
     return (
-      <div className="testing">
-        <h1>LandingPage</h1>
-        <button> Login </button>
-        <button> Signup </button>
+      <div>
+        <nav id="navbar" className="navbar navbar-toggleable-md navbar-light bg-faded">
+          <button
+            className="navbar-toggler navbar-toggler-right"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div id="hiddentitle" className="navbar-brand" />
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul id="navbarNavDropdown" className="navbar-nav">
+              <li id="login-container" className="nav-link ml-auto" href="www.google.com">
+                <button id="login">Login</button>
+              </li>
+              <li id="signup-container" className="nav-link" href="www.google.com">
+                <button id="signup">Sign Up</button>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <h1 id="title" > Tech Teams Landing Page </h1>
       </div>
     );
   }
