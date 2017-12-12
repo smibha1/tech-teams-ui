79a864105dfa9d42e45b8a5fd84f3f0187b3fbdc
Kai An David Lee <david.ka.lee@gmail.com>
2017-12-12 15:25:20 -0800

    [fix] - Change routes by using URL

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 /* ------------------ SERVER  ------------------ */
-let app = express();
+const app = express();
 // Serve up the static files!
-app.use(express.static(__dirname + '/../client/dist'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(cors);
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors);
+
+app.use('/', express.static(path.join(__dirname, '/../client/dist')));
+// app.use('/api', router)
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/dist/index.html'));
+});
 
 const port = 3001;
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`Server Starts on ${port}`);
 });
