7cbdef60ffabb1791a981b8d01481be0d9558ac9
“Smita <smitajbhakta@gmail.com>
2017-12-12 11:50:14 -0800

    [fix] -CreateProject as a component of navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import swal from 'sweetalert2';
-import $ from 'jquery';
 import './NavBar.css';
 import Projects from '../../containers/Projects/ProjectsContainer';
-
+import CreateProject from '../NavBar/CreateProject/CreateProject';
 
 class NavBar extends React.Component {
   handleAcceptClick() {
@@ -12,133 +10,6 @@ class NavBar extends React.Component {
 
   handleRejectClick() {
     console.log('reject click', this.props);
-  }
-
-
-  handleCreateProjectClick() {
-    console.log('click on createProject', this.props);
-    swal.setDefaults({
-      showCancelButton: true,
-      confirmButtonText: 'Next &rarr;',
-      progressSteps: ['1', '2', '3', '4'],
-    });
-
-    const steps = [
-      {
-        title: 'Basic Info',
-        text: 'ProjectName',
-        width: 800,
-        padding: 100,
-        animation: false,
-        html: '<form action="/action_page.php">'
-          + 'Project Name: &nbsp;'
-          + '<input id:"projName" type="text" name="firstname" placeholder="DevDates"><br>'
-          + '<br>'
-          + 'Headquarters: &nbsp;'
-          + '<input id:"headquarters" type="text" name="lastname" placeholder="Los Angeles, CA"><br>'
-          + '<br>'
-          + 'Company Logo: &nbsp;'
-          + '<input id:"logo" type="url" name="lastname" placeholder="http://i.telegraph.co.uk/logo.jpg"><br>'
-          + '<br>'
-          + 'Project Description: &nbsp;'
-          + '<input id:"projDesc" type="text" name="lastname" placeholder="Bringing Devs together"><br>'
-          + '</form>',
-        focusConfirm: false,
-        preConfirm: () => [
-          $('#projName').val(),
-          $('#logo').val(),
-          $('#headquarters').val(),
-          $('#projDesc').val(),
-        ],
-      },
-      {
-        title: 'Project Links',
-        width: 800,
-        padding: 100,
-        animation: false,
-        html: '<form action="/action_page.php">'
-        + 'Your Project Website: &nbsp;'
-        + '<input id:"companyWebsite" type="url" name="firstname" placeholder="www.devdates.com"><br>'
-        + '<br>'
-        + '<i class="fa fa-linkedin-square" aria-hidden="true"></i> &nbsp;'
-        + '<input id:"linkedin" type="url" name="lastname" placeholder="https://linkedin.com/tonystark"><br>'
-        + '<br>'
-        + '<i class="fa fa-slack" aria-hidden="true"></i> &nbsp;'
-        + '<input id:"slack" type="url" name="lastname" placeholder="https://hrla-students.slack.com/messages"><br>'
-        + '<br>'
-        + '<i class="fa fa-github" aria-hidden="true"></i> &nbsp;'
-        + '<input id:"github" type="url" name="lastname" placeholder="https://github.com/tonystark"><br>'
-        + '<br>'
-        + '<i class="fa fa-trello" aria-hidden="true"></i> &nbsp;'
-        + '<input id:"trello" type="url" name="lastname" placeholder="https://trello.com/b/0NYefHQ3/devdates"><br>'
-        + '</form>',
-        focusConfirm: false,
-        preConfirm: () => [
-          $('#companyWebsite').val(),
-          $('#linkedin').val(),
-          $('#slack').val(),
-          $('#github').val(),
-          $('#trello').val(),
-        ],
-      },
-      {
-        title: 'Tech Stack',
-        width: 800,
-        padding: 100,
-        animation: false,
-        html: '<form action="/action_page.php">'
-          + '<input id:"techStack" class="submissionfield" type="text" name="firstname" placeholder="React, Node, MongoDB"><br>'
-          + '</form>',
-        focusConfirm: false,
-        preConfirm: () => [
-          $('#techStack').val(),
-        ],
-      },
-      {
-        title: 'Add Team Members',
-        width: 800,
-        padding: 100,
-        animation: false,
-        html: '<form action="/action_page.php">'
-          + 'Name: &nbsp;'
-          + '<input id:"name1" type="text" name="firstname" placeholder="Tony Stark"> &nbsp;'
-          + 'Position: &nbsp;'
-          + '<input id:"position1" type="text" name="firstname" placeholder="Product Owner"><br>'
-          + '<br>'
-          + 'Name: &nbsp;'
-          + '<input id:"name2" type="text" name="firstname" placeholder="Tony Stark"> &nbsp;'
-          + 'Position: &nbsp;'
-          + '<input id:"position2" type="text" name="firstname" placeholder="Developer"><br>'
-          + '<br>'
-          + 'Name: &nbsp;'
-          + '<input id:"name3" type="text" name="firstname" placeholder="Pepper Potts"> &nbsp;'
-          + 'Position: &nbsp;'
-          + '<input id:"position3" type="text" name="firstname" placeholder="Scrum Master"><br>'
-          + '</form>',
-        focusConfirm: false,
-        preConfirm: () => [
-          $('#name1').val(),
-          $('#name2').val(),
-          $('#name3').val(),
-          $('#position1').val(),
-          $('#position2').val(),
-          $('#position3').val(),
-        ],
-      },
-    ];
-
-    swal.queue(steps).then((result) => {
-      swal.resetDefaults();
-      console.log('result', result);
-      if (result.value) {
-        swal({
-          title: 'All done!',
-          type: 'success',
-          showConfirmButton: false,
-          timer: 1000,
-        });
-      }
-    });
   }
 
 
@@ -169,27 +40,8 @@ class NavBar extends React.Component {
                   <span className="fa fa-search" aria-hidden="true" />
                 </button>
               </form>
-              <li
-                id="createProj-container"
-                className="nav-link toggle ml-auto"
-                href="www.google.com"
-              >
-                <i
-                  id="big-bell-navbar"
-                  className="fa fa-plus fa-2x pt-2 mt-lg-0"
-                  aria-hidden="true"
-                  onClick={() => this.handleCreateProjectClick()}
-                />
-              </li>
-              <li className="navbar-text visible-xs-inline-block">
-                <i
-                  id="dropdownPlus"
-                  className="fa fa-plus pt-2 mt-lg-0"
-                  aria-hidden="true"
-                  onClick={() => this.handleCreateProjectClick()}
-                />
-                Create Project
-              </li>
+
+              <CreateProject />
               <li id="alerts-container" className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-menu-right mt-4 mt-lg-0"
--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // import React from 'react';
 // import DevInfoContainer from '../../containers/Profile/DevInfoContainer';
 // import TechnicalSkillsContainer from '../../containers/Profile/TechnicalSkillsContainer';
@@ -17,3 +18,24 @@
 // );
 //
 // export default Profile;
+=======
+import React from 'react';
+import DevInfoContainer from '../../containers/Profile/DevInfoContainer';
+import TechnicalSkillsContainer from '../../containers/Profile/TechnicalSkillsContainer';
+// import CurrentProjectsContainer from '../../containers/Profile/CurrentProjectsContainer';
+// import CompletedProjectsContainer from '../../containers/Profile/CompletedProjectsContainer';
+
+const Profile = () => (
+  <div>
+    <DevInfoContainer />
+    <br />
+    <TechnicalSkillsContainer />
+    <br />
+    {/* <CurrentProjectsContainer />
+    <br />
+    <CompletedProjectsContainer /> */}
+  </div>
+);
+
+export default Profile;
+>>>>>>> [fix]
