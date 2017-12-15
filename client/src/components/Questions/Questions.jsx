5d233eb4fe0f56e4a6b95b4ed9c3fdd08a6e1a73
“Smita <smitajbhakta@gmail.com>
2017-12-15 12:21:42 -0800

    [feat] -Questions can take in inputs

--- client/src/components/Questions/Questions.jsx
+++ client/src/components/Questions/Questions.jsx
@@ -5,108 +5,61 @@ import TechnicalSkillsContainer from '../../containers/Profile/TechnicalSkillsCo
 
 class Questions extends React.Component {
   handleTitle1Change(e) {
-    console.log(e.target.value);
+    console.log('target value',e.target);
   }
 
   render() {
     return (
       <div >
         <h1>Getting Started</h1>
-        <div className="container">
-          <form className="form-horizontal" method="post" onSubmit={this.handleTitle1Change}>
-            <div className="row">
-              <div className="form-group row col-lg-6">
-                <div className="col-21">
-                  <label className="custom-control custom-radio">
-                    <input
-                      id="radioDev"
-                      name="cat"
-                      type="radio"
-                      className="custom-control-input"
-                      value="0"
-
-                    />
-                    <span className="custom-control-indicator" />
-                    <span className="custom-control-description">Developer</span>
-                  </label>
-                  <label className="custom-control custom-radio">
-                    <input
-                      id="radioSM"
-                      name="cat"
-                      type="radio"
-                      className="custom-control-input"
-                      value="1"
-                    />
-                    <span className="custom-control-indicator" />
-                    <span className="custom-control-description">Scrum Master</span>
-                  </label>
-                  <label className="custom-control custom-radio">
-                    <input
-                      id="radioPM"
-                      name="cat"
-                      type="radio"
-                      className="custom-control-input"
-                      value="2"
-                    />
-                    <span className="custom-control-indicator" />
-                    <span className="custom-control-description">Product Owner</span>
-                  </label>
-                  <div className="form-group row">
-                    <label
-                      htmlFor="example-text-input"
-                      className="col-2 col-form-label"
-                    >
-                      Other
-                    </label>
-                    <div className="col-5">
-                      <input
-                        className="form-control"
-                        type="text"
-                        value="UI/UX"
-                        id="example-text-input"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="form-group row">
-              <label className="col-2 col-form-label">Location: </label>
-              <div className="col-10">
-                <input
-                  className="form-control"
-                  type="text"
-                  value="Los Angeles, CA"
-                  id="example-text-input"
-                />
-              </div>
+        <form onSubmit={this.handleTitle1Change}>
+          <fieldset className="legend-brdr-bttm" >
+          <div className="radio-inline">
+          <label className="custom-control custom-radio">
+            <input id="scrumMaster" name="radio" type="radio" className="custom-control-input"/>
+            <span className="custom-control-indicator"></span>
+            <span className="custom-control-description">Scrum Master</span>
+          </label>
+          <label className="custom-control custom-radio">
+            <input id="developer" name="radio" type="radio" className="custom-control-input"/>
+            <span className="custom-control-indicator"></span>
+            <span className="custom-control-description">Developer</span>
+          </label>
+          <label className="custom-control custom-radio">
+            <input id="projectOwner" name="radio" type="radio" className="custom-control-input"/>
+            <span className="custom-control-indicator"></span>
+            <span className="custom-control-description">Project Owner</span>
+          </label>
+          <label className="custom-control custom-radio">
+            <input id="otherRadio" name="radio" type="radio" className="custom-control-input"/>
+            <span className="custom-control-indicator"></span>
+            <span className="custom-control-description">Other</span>
+            <span className="form-group">
+              <label htmlFor="Other"></label>
+              <input type="text" id="other" className="form-control" placeholder="UI/UX"/>
+            </span>
+          </label>
+          </div>
+            <div className="form-group">
+              <label htmlFor="Location">Location</label>
+              <input type="text" id="location" className="form-control" placeholder="Los Angeles, CA"/>
             </div>
             <TechnicalSkillsContainer />
-            <div className="form-group row">
-              <label className="col-2 col-form-label">About Me: </label>
-              <div className="col-10">
-                <input
-                  className="form-control"
-                  type="text"
-                  value="I am looking for a full time position in react."
-                  id="example-text-input"
-                />
-              </div>
+            <div className="form-group">
+              <label htmlFor="AboutMe">About Me</label>
+              <input type="text" id="aboutMe" className="form-control" placeholder="Tell us about yourself"/>
             </div>
-            <label className="custom-control custom-checkbox">
-              <input type="checkbox" className="custom-control-input" />
-              <span className="custom-control-indicator" />
-              <span className="custom-control-description">Check this custom checkbox</span>
-            </label>
-            <div className="form-group row">
-              <div className="offset-sm-2 col-sm-10">
-                <Link to="/username/profile" href="/username/profile">
-                  <button type="submit" className="btn btn-primary">Create Profile</button>
-                </Link>
-              </div>
+            <div className="checkbox">
+              <label>
+                <input id="available" type="checkbox"/> Available to Join a Team
+              </label>
             </div>
-          </form>
-        </div>
+            <Link to = "/username/profile">
+            <button type="submit" className="btn btn-primary" onClick={this.handleTitle1Change}>Create Profile</button>
+            </Link>
+          </fieldset>
+        </form>
+        
       </div>
     );
   }
