f4b2d6954fb9ccfd7cc800cf0ee8028e52a201f8
Jake Perez <jacobanthony.perez@gmail.com>
2017-12-18 15:50:30 -0800

    [ops] - posts to the server

--- client/src/components/Questions/Questions.jsx
+++ client/src/components/Questions/Questions.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './Questions.css';
 import TechnicalSkillsContainer from '../../containers/Profile/TechnicalSkillsContainer';
-<<<<<<< HEAD
 import {
   Table,
   TableBody,
@@ -10,24 +9,22 @@ import {
   TableHeader,
   TableHeaderColumn,
   TableRow,
-  TableRowColumn,
+  TableRowColumn
 } from 'material-ui/Table';
 import TextField from 'material-ui/TextField';
 import Toggle from 'material-ui/Toggle';
-=======
 import swal from 'sweetalert2';
 import $ from 'jquery';
->>>>>>> [ops]
 
 class Questions extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
   handleQuestionsSubmit(e) {
     e.preventDefault();
-    // let email = props.email;
-    // let name = props.name;
+    // let email = this.props.email;
+    // let name = this.props.name;
 
     let positionObj = {
       0: 'Scrum Master',
@@ -47,7 +44,7 @@ class Questions extends React.Component {
       name: 'full name',
       location: location,
       aboutMe: aboutMe,
-      available: available === 'on',
+      available: available === 'on'
     }
     if (!(location && aboutMe) || key === -1 || (key === 3 && !other)) {
       alert('PLEASE FILL EVERTHING OUT')
@@ -64,19 +61,10 @@ class Questions extends React.Component {
   }
 
   render() {
-<<<<<<< HEAD
-    return (
-      <div id="overlayQuestions">
-      <div id="questions-container">
-        <h1>Getting Started</h1>
-        <form onSubmit={this.handleTitle1Change}>
-          <fieldset className="legend-brdr-bttm" >
-=======
     return (<div id="questions-container">
       <h1>Getting Started</h1>
       <form onSubmit={this.handleQuestionsSubmit}>
         <fieldset className="legend-brdr-bttm">
->>>>>>> [ops]
           <div className="radio-inline">
             <label className="custom-control custom-radio">
               <input id="scrumMaster" name="radio" type="radio" className="custom-control-input"/>
@@ -103,38 +91,6 @@ class Questions extends React.Component {
               </span>
             </label>
           </div>
-<<<<<<< HEAD
-            <div className="form-group">
-              <label htmlFor="Location">Location</label>
-              <input type="text" id="location" className="form-control" placeholder="Los Angeles, CA"/>
-            </div>
-            <TechnicalSkillsContainer />
-            <div className="form-group">
-              <label htmlFor="AboutMe">About Me</label>
-              <input type="text" id="aboutMe" className="form-control" placeholder="Tell us about yourself"/>
-            </div>
-            <div className="checkbox">
-              <label>
-                <input id="available" type="checkbox"/> Available to Join a Team
-              </label>
-            </div>
-            <label class="switch">
-
-  <input className="checkbox" type="checkbox"/>
-  
-  <span id="available" class="slider round">
-  
-  </span>
-</label>
-            <Link to = "/username/profile">
-            <button type="submit" className="btn btn-primary">Create Profile</button>
-            </Link>
-          </fieldset>
-        </form>
-        </div>
-      </div>
-    );
-=======
           <div className="form-group">
             <label htmlFor="Location">Location</label>
             <input type="text" id="location" className="form-control" placeholder="Los Angeles, CA"/>
@@ -145,17 +101,23 @@ class Questions extends React.Component {
             <input type="text" id="aboutMe" className="form-control" placeholder="Tell us about yourself"/>
           </div>
           <div className="checkbox">
+            <label className="switch">
+              <input id="available" className="checkbox" type="checkbox"/>
+              <span  className="slider round"></span>
+            </label>
             <label>
-              <input id="available" type="checkbox"/>
               Available to Join a Team
             </label>
           </div>
-          <button type="submit" className="btn btn-primary">Create Profile</button>
+          {
+            // <Link to="/username/profile">
+            <button type="submit" className="btn btn-primary">Create Profile</button>
+            // </Link>
+          }
         </fieldset>
       </form>
-
     </div>);
->>>>>>> [ops]
+
   }
 }
 
