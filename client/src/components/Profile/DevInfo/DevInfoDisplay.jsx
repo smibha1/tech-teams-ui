dcb799bbf1ee0768ec241b8020b46961fb9bc1b8
Jake Perez <jacobanthony.perez@gmail.com>
2017-12-20 12:19:47 -0800

    [ops] - set headers - render profile info accordingly

--- client/src/components/Profile/DevInfo/DevInfoDisplay.jsx
+++ client/src/components/Profile/DevInfo/DevInfoDisplay.jsx
@@ -1,22 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
-const DevInfoDisplay = props => (
-  <div id="profileInfo-container">
-    <img id="profileImg" src={props.info.devPicture} alt={props.info.devName} height="200" /> <br />
-    <div id="profileUsername"> {props.info.devName} </div> <br />
-    {props.info.devPosition} || {props.info.devLocation} <br />
-    <div id="divider"> </div>
+class DevInfoDisplay extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userPicture: '',
+      userName: '',
+      userPosition: '',
+      userLocation: '',
+      userDescription: '',
+      userAvailability: ''
+    }
+  }
 
-    {props.info.devDesc}
-  </div>
+  componentWillMount() {
+    // axios({
+    // get prolife mofo
+    // jwtDecode(token) for Email
+    // send that shit in obj;
+    // ask for user shit
 
-);
+        // })
+
+
+  }
+
+  render() {
+    return (<div id="profileInfo-container">
+      <img id="profileImg" src={this.state.userPicture} alt={this.state.userName} height="200"/>
+      <br/>
+      <div id="profileUsername">
+        {this.state.userName}
+      </div>
+      <br/> {this.state.userPosition}
+      || {this.state.userLocation}
+      <br/>
+      <div id="divider">
+        {this.state.userDescription}
+
+      </div>
+        {this.state.userAvailability}
+    </div>)
+
+  }
+}
+
 
 export default DevInfoDisplay;
 
 DevInfoDisplay.propTypes = {
-  info: PropTypes.oneOfType([
-    PropTypes.object,
-  ]).isRequired,
+  info: PropTypes.oneOfType([PropTypes.object]).isRequired
 };
